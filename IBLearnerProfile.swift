58376097103edf71bd3604c240ad9032b481aba5
cJoyce04 <36705102+cJoyce04@users.noreply.github.com>
2018-04-12 15:08:51 -0400

    cJoyce
    IBLearnerProfile

--- IBLearnerProfile.swift
+++ IBLearnerProfile.swift
@@ -2,38 +2,35 @@ struct QuestionAnswerer {
     func responseTo(question: String) -> String {
         let lowerQuestion = question.lowercased()
         
-        if lowerQuestion.contains("how") && lowerQuestion.contains("ib program") && lowerQuestion.contains("different") && lowerQuestion.contains("other programs") || lowerQuestion.contains("why") && lowerQuestion.contains("take") && lowerQuestion.contains("computer science") || lowerQuestion.contains("why") && lowerQuestion.contains("choose") && lowerQuestion.contains("computer science") {
-            return "You should join the computer science class because it will help prepare you and teach you about a language that will likely dominate our future lives. This class expands your knowledge of the inner working of all of the technologies that currently surround you in your everyday life."
+        if lowerQuestion.contains("how") && lowerQuestion.contains("ib program") && lowerQuestion.contains("different") && lowerQuestion.contains("other programs") || lowerQuestion.contains("what makes") && lowerQuestion.contains("ib program") && lowerQuestion.contains("different") || lowerQuestion.contains("what makes") && lowerQuestion.contains("ib program") && lowerQuestion.contains("special") || lowerQuestion.contains("what makes") && lowerQuestion.contains("ib program") && lowerQuestion.contains("better") {
+            return "This IB program is different from most other school’s programs as it focuses on 7 main categories in order to build your knowledge: the arts, the sciences, sciences, mathematics, individuals and societies, studies in language and literature, language acquisition and international mindedness. It is focused on not only expanding your knowledge of the ‘whats’ of the world, but it explores the ‘whys’ of the world."
             
-        } else if lowerQuestion.contains("should") && lowerQuestion.contains("switch") && lowerQuestion.contains("computer science") || lowerQuestion.contains("should") && lowerQuestion.contains("take") && lowerQuestion.contains("computer science") || lowerQuestion.contains("should") && lowerQuestion.contains("join") && lowerQuestion.contains("computer science") {
-            return "If you are interested to learn about the logic and method behind programmes, computers, robots and other technologies, computer science is the perfect class for you."
+        } else if lowerQuestion.contains("why") && lowerQuestion.contains("international school") && lowerQuestion.contains("ib program") && lowerQuestion.contains("special") || lowerQuestion.contains("why") && lowerQuestion.contains("international school") && lowerQuestion.contains("ib program") && lowerQuestion.contains("better") {
+            return "The Atlanta International School program is special because it focuses on building its student’s characters and ensuring that in the process of learning about educational material, they are also discovering themselves and identifying the ways that they best learn so that they can apply it to their lives in the future."
             
-        } else if lowerQuestion.contains("what") && lowerQuestion.contains("language") && lowerQuestion.contains("learn") && lowerQuestion.contains("computer science") || lowerQuestion.contains("what") && lowerQuestion.contains("language") && lowerQuestion.contains("available") && lowerQuestion.contains("computer science") || lowerQuestion.contains("what") && lowerQuestion.contains("language") && lowerQuestion.contains("taught") && lowerQuestion.contains("computer science") || lowerQuestion.contains("what") && lowerQuestion.contains("language") && lowerQuestion.contains("options") && lowerQuestion.contains("computer science") {
-            return "In each of the different grades, there are units that are mainly focused on a specific programming language. For example, in 6th grade, the learning is focused on the basic foundation of coding, in 7th grade, the learning is focused on javascript and in 8th grade the learning is focused on swift. However, there are also different parts of the units that allow personal exploration for the student to choose the coding language that best fits the type of programmes they want to build."
+        } else if lowerQuestion.contains("what") && lowerQuestion.contains("ib") && lowerQuestion.contains("profile") && lowerQuestion.contains("characteristic") || lowerQuestion.contains("what") && lowerQuestion.contains("ib") && lowerQuestion.contains("profile") && lowerQuestion.contains("trait") {
+            return "There are 10 different IB learner traits: Inquirer, knowledgeable, thinker, communicator, principled, open minded, caring, risk taker, balanced and reflective."
             
-        } else if lowerQuestion.contains("when") && lowerQuestion.contains("is") && lowerQuestion.contains("computer science") {
-            return "The computer science class will fit into any students 9 class rotation schedule."
+        } else if lowerQuestion.contains("what") && lowerQuestion.contains("ib") && lowerQuestion.contains("program") {
+            return "The IB learner program was a learning system that was developed in order to make a students experience during the education process more enriched."
             
-        } else if lowerQuestion.contains("when") && lowerQuestion.contains("can") && lowerQuestion.contains("computer science") && lowerQuestion.contains("take") {
-            return "Any student that is either in any grade above 5th grade can take computer science as one of their option classes."
+        } else if lowerQuestion.contains("what") && lowerQuestion.contains("different parts") && lowerQuestion.contains("ib learner") && lowerQuestion.contains("what") && lowerQuestion.contains("different stages") && lowerQuestion.contains("ib learner") {
+            return "There are 3 different stages of the IB learner program: PYP: Primary Year Program - MYP: Middle Years Program -  DP: Diploma Program. The IB learner program is a program for 3-19 year old children."
             
-        } else if lowerQuestion.contains("what") && lowerQuestion.contains("is computer science") || lowerQuestion.contains("what") && lowerQuestion.contains("computer science") && lowerQuestion.contains("about") {
-            return "Computer science is the study of modern technologies and how they impact our everyday lives."
+        } else if lowerQuestion.contains("i benefit") && lowerQuestion.contains("ib") && lowerQuestion.contains("system") || lowerQuestion.contains("my child benefit") && lowerQuestion.contains("ib") && lowerQuestion.contains("system") || lowerQuestion.contains("take from") && lowerQuestion.contains("ib") && lowerQuestion.contains("system") {
+            return "The IB learner program helps enhance young minds with positive attributes to the community around them to make exceptional contributions to the campus, and later, their lives. By taking the IB learner system, you have the opportunity to understand more than just the basics, but rather the whole on an international point of view."
             
-        } else if lowerQuestion.contains("how") && lowerQuestion.contains("computer science") && lowerQuestion.contains("future") {
-            return "Computer science will help you better your skills on a computer so that in the future, whether or not you choose a career that involves computer science like a data analyst, IT consultant, multimedia programmer or if you decide to do something else, any career will almost always involve some work on a computer. It is important to study computer science so that you can be efficient on a computer either way."
+        } else if lowerQuestion.contains("how") && lowerQuestion.contains("ais enforce") && lowerQuestion.contains("ib traits") || lowerQuestion.contains("how") && lowerQuestion.contains("ib traits included") && lowerQuestion.contains("student") && lowerQuestion.contains("ais") {
+            return "Atlanta International School consists of 15 grades: 3k, 4k, 5k, 1st - 12th. Even the youngest members of the AIS community are required to learn and understand the morals in which they must follow, throughout their upcoming years at AIS, these are the IB learner traits. Activities, punishments, and praise are shown to any student who acts accordingly. Ask: what are the ib learner profile traits for more information."
             
-        } else if lowerQuestion.contains("does") && lowerQuestion.contains("computer science") && lowerQuestion.contains("relate") && lowerQuestion.contains("class") || lowerQuestion.contains("computer science") && lowerQuestion.contains("help") && lowerQuestion.contains("subject") {
-            return "Computer science is a great way of learning the in’s and out’s of your computer so that you can be more efficient in your learning for other classes when you are required to use your laptop."
+        } else if lowerQuestion.contains("where") && lowerQuestion.contains("america") && lowerQuestion.contains("ib education") {
+            return "IB schools around America are situated in Georgia, Tennessee, North Carolina, Florida, Illinois, Minnesota, Denver, New Mexico, Arizona, and California."
             
-        } else if lowerQuestion.contains("when") && lowerQuestion.contains("is") && lowerQuestion.contains("computer science") {
-            return "The computer science class will fit into any students 9 class rotation schedule."
+        } else if lowerQuestion.contains("what") && lowerQuestion.contains("aim") && lowerQuestion.contains("child") && lowerQuestion.contains("ib learner") || lowerQuestion.contains("child") && lowerQuestion.contains("ib learner") && lowerQuestion.contains("community") {
+            return "The IB program is different from most other school’s programs as it focuses on 7 main categories in order to build your knowledge: the arts, the sciences, sciences, mathematics, individuals and societies, studies in language and literature, language acquisition and international mindedness. It is focused on not only expanding your knowledge of the ‘whats’ of the world, but it explores the ‘whys’ of the world."
             
-        } else if lowerQuestion.contains("need") && lowerQuestion.contains("equipment") && lowerQuestion.contains("computer science") || lowerQuestion.contains("need") && lowerQuestion.contains("material") && lowerQuestion.contains("computer science") {
-            return "No special equipment is required for this class. The only thing that you will need, which is already a school requirement, is a computer."
-            
-        } else if lowerQuestion.contains("what") && lowerQuestion.contains("do") && lowerQuestion.contains("in computer science") {
-            return "In computer science, a variety of things are done involving computers to expand your knowledge on their impact on our everyday lives and the future. (We also code!)"
+        } else if lowerQuestion.contains("who") && lowerQuestion.contains("ib education"){
+            return "The International Baccalaureate® (IB) was founded in Geneva, Switzerland in 1968, originally as a non-profit educational system. As the IB started to grow, several other international schools pitched in to start the IB Diploma Program."
             
         } else {
             return "I am sorry, I am not sure I understand your question."
